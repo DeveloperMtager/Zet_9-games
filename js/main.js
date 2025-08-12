a7a1dc6501bb772527d0091ea459cf635cb7be62
Z9_Games <Mtagir999@gmail.com>
2025-08-12 14:56:55 +0300

    Delete js directory

--- js/main.js
+++
@@ -1,8 +0,0 @@
-// Главный скрипт для инициализации, объединения логики и т.п.
-
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Сайт Zet_9-games загружен');
-  
-  // Можно сразу загрузить нужную игру
-  // loadGame('games/game1/index.html');
-});
