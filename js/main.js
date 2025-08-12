775442fa49ee23967a80b2bc19086a2146b0376f
DeveloperMtager <Mtagir999@gmail.com>
2025-08-12 14:34:24 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Главный скрипт для инициализации, объединения логики и т.п.
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Сайт Zet_9-games загружен');
+  
+  // Можно сразу загрузить нужную игру
+  // loadGame('games/game1/index.html');
+});
